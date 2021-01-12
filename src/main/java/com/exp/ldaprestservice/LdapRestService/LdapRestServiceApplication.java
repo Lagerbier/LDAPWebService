ac97ce1868e8edcf7f7aea9d43a20026d4fa37d5
Jas <55251065+jaswpratt@users.noreply.github.com>
2021-01-12 08:57:11 -0500

    Add files via upload

---
+++ src/main/java/com/exp/ldaprestservice/LdapRestService/LdapRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.exp.ldaprestservice.LdapRestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exp/ldaprestservice/LdapRestService/LdapRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exp.ldaprestservice.LdapRestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
